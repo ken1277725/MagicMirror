5cb64a2fb71d78f8870bef0e14a717ab36dada96
YuanChingLin <ken1277725>
2019-03-31 01:41:48 +0800

    a
    as title

--- python-script/communication.py
+++ python-script/communication.py
@@ -10,9 +10,6 @@ class sender():
 
     def send(self, params):
 
-        params = {'sessionKey': '9ebbd0b25760557393a43064a92bae539d962103',
-                  'format': 'xml', 'platformId': 1}
-
         r = requests.post(self.url,
                           headers=self.headers, params=params)
 
