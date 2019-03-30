12ceeaa69862bfc3dcbd99c3ec9c5f317a040830
YuanChingLin <ken1277725>
2019-03-30 20:31:51 +0800

    bus bugger 1st version

--- modules/Taiwan-Bus/Taiwan-Bus.js
+++ modules/Taiwan-Bus/Taiwan-Bus.js
@@ -170,33 +170,33 @@ Module.register("Taiwan-Bus", {
 	 */
 	getLocaleSpecification: function (timeFormat) {
 		switch (timeFormat) {
-			case 12:
-				{
-					return {
-						longDateFormat: {
-							LT: "h:mm A"
-						}
-					};
-					break;
+		case 12:
+		{
+			return {
+				longDateFormat: {
+					LT: "h:mm A"
 				}
-			case 24:
-				{
-					return {
-						longDateFormat: {
-							LT: "HH:mm"
-						}
-					};
-					break;
+			};
+			break;
+		}
+		case 24:
+		{
+			return {
+				longDateFormat: {
+					LT: "HH:mm"
 				}
-			default:
-				{
-					return {
-						longDateFormat: {
-							LT: moment.localeData().longDateFormat("LT")
-						}
-					};
-					break;
+			};
+			break;
+		}
+		default:
+		{
+			return {
+				longDateFormat: {
+					LT: moment.localeData().longDateFormat("LT")
 				}
+			};
+			break;
+		}
 		}
 	},
 	/* broadcastEvents()
